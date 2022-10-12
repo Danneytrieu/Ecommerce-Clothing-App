c250b46d1eeaf628d93e632b58dc51a2631cfa84
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2022-10-12 12:28:07 -0600

    created signup form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn =()=>{
   //Google Login
   const logGoogleUser = async () => {
@@ -14,7 +16,8 @@ const SignIn =()=>{
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser} >Sign in with Google Popup</button>
+      <SignUpForm/>
     </div>
   )
 }
-export default SignIn
+export default SignIn 
